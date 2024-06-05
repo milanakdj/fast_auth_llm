43783d015d96cc05f63c162a68059941e19fb498
milanakdj <milanakdj@gmail.com>
2024-06-05 16:30:25 +0545

    right aligned a div

---
+++ client.py
@@ -0,0 +1,26 @@
+import requests
+
+bearer_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiaWpheTU2MjciLCJpZCI6MiwiZXhwIjoxNzE3NTMwMDYyfQ.OwFF1h10bKV-REkOCtsGYYxf27ngIPNaQoFvkVWU5Ss'
+header = {
+    "Authorization": f"Bearer {bearer_token}"
+}
+
+form_data = {
+    "prompt": (None, "Who are you?")
+}
+
+# Send the request with form data
+response = requests.post(
+    "https://spiralogics.ai/samchat/ask",
+    files=form_data,  # Use files to send multipart/form-data
+    headers=header
+)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(response.content)
+
+ 
+
